18e6135b84004046232113da0a178b1a5b971161
wsw0108 <wsw0108@gmail.com>
2021-06-19 12:37:21 +0800

    fix xmake.lua for macosx

--- xmake.lua
+++ xmake.lua
@@ -4,6 +4,7 @@ set_languages("cxx17")
 set_warnings("all", "all")
 -- set_warnings("all", "error")
 
+if is_plat("windows") then
 target("ColorPicker")
     set_kind("binary")
     add_files("src/*.cxx", "src/Windows/*.cxx")
@@ -15,12 +16,16 @@ target("ColorPicker")
     add_defines("OS_WINDOWS", "UNICODE", "RELEASE")
     -- add_cxxflags("/Zi")
     add_links("kernel32", "user32", "gdi32", "ole32")
+end
 
-target("ColorPicker-macOS")
+if is_plat("macosx") then
+target("ColorPicker")
     set_kind("binary")
-    add_files("src/*.cxx", "src/macOS/*.cxx")
+    add_files("src/*.cxx", "src/macOS/*.cxx", {cxflags = "-x objective-c++"})
     on_load("xxdmac")
     add_includedirs("src", "src/macOS")
     add_defines("OS_MACOS", "RELEASE")
     add_cxflags("-mmacosx-version-min=10.11")
+    -- add_cxflags("-x objective-c++")
     add_frameworks("AppKit", "IOKit")
+end
